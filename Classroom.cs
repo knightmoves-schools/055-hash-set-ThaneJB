d121c3a7a463f61484c4333c2af2a5eb2f6eb932
Thane <thabay0606@students.maquoketaschools.org>
2026-05-05 14:25:19 -0500

    Didn't uppercase the S in HashSet.

--- Classroom.cs
+++ Classroom.cs
@@ -4,7 +4,7 @@ public class Classroom{
 
     public HashSet<string> CreateTeam(string first, string second, string third){
         
-        return new Hashset<string>{first, second, third};
+        return new HashSet<string>{first, second, third};
         
     }
 }
